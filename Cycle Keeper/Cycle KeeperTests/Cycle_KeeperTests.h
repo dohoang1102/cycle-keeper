8975712bad5b5602c33205e4eec1ac531285e872
David Edwards <david@rovidica.com>
2012-05-30 23:00:30 -0600

    First night, created project
    added custom font.

---
+++ Cycle Keeper/Cycle KeeperTests/Cycle_KeeperTests.h
@@ -0,0 +1,13 @@
+//
+//  Cycle_KeeperTests.h
+//  Cycle KeeperTests
+//
+//  Created by David Edwards on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Cycle_KeeperTests : SenTestCase
+
+@end
---
+++ Cycle Keeper/Cycle KeeperTests/Cycle_KeeperTests.m
@@ -0,0 +1,32 @@
+//
+//  Cycle_KeeperTests.m
+//  Cycle KeeperTests
+//
+//  Created by David Edwards on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Cycle_KeeperTests.h"
+
+@implementation Cycle_KeeperTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Cycle KeeperTests");
+}
+
+@end
